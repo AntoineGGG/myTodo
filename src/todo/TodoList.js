c352a8b39ad69060a2157c84726e636326f7a331
AntoineGGG <a.gorenflot@gmx.fr>
2020-11-08 14:34:31 +0100

    create a Todolist with material UI and bootstrap

--- src/todo/TodoList.js
+++ src/todo/TodoList.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Button } from '@material-ui/core';
+import './TodoList';
 
 class TodoList extends Component {
   constructor() {
@@ -13,23 +15,65 @@ class TodoList extends Component {
     this.setState({ userInput: event.target.value });
   };
 
-  addTodo = (event) => {};
+  addTodo = (event) => {
+    event.preventDefault();
+    this.setState({
+      userInput: '',
+      items: [...this.state.items, this.state.userInput],
+    });
+  };
+
+  showTodo = () => {
+    return this.state.items.map((value) => {
+      return (
+        <div>
+          <div className='list-group-item' key={value}>
+            {value} |{' '}
+          </div>
+          <Button
+            variant='contained'
+            color='secondary'
+            onClick={this.deleteTasks}
+          >
+            X
+          </Button>
+        </div>
+      );
+    });
+  };
+
+  deleteTasks = (event) => {
+    event.preventDefault();
+    let filteredArray = this.state.items;
+    const index = filteredArray.indexOf(event.target.value);
+    filteredArray.splice(index, 1);
+    this.setState({
+      items: filteredArray,
+    });
+  };
 
   render() {
     return (
       <div>
-        <h1>My TodoList</h1>
-        <form>
+        <h1 align='center'>My TodoList</h1>
+        <form className='form-row align-items-center'>
           <input
+            className='form-control mb-2'
             value={this.state.userInput}
             type='text'
             placeholder='Renseigner un item'
             onChange={this.handleChange}
           />
-          <button onClick={this.addTodo} type='submit'>
+          <Button
+            variant='contained'
+            color='primary'
+            onClick={this.addTodo}
+            type='submit'
+          >
             Add
-          </button>
+          </Button>
         </form>
+        <div className='List-group'>{this.showTodo()}</div>
       </div>
     );
   }
